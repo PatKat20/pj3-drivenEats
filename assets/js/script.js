4343811ab7f0b09790b3bbfcebbb4777b2dae4d9
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-01-03 10:50:31 -0300

    Finaliza selecao de itens

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,34 @@ const dishes = document.querySelectorAll('.dish')
 const drinks = document.querySelectorAll(".drink")
 const desserties = document.querySelectorAll(".dessert")
 
+// Funções
+const verifyCheckedInput = foodArray => {
+    foodArray.forEach(input =>{
+        if(input.checked){
+            input.parentElement.classList.add("selecionado")
+        } else{
+            input.parentElement.classList.remove("selecionado")
+        }
+    })
+}
 
+
+
+// Eventos
+productList.addEventListener("click", _=>{
+    verifyCheckedInput(dishes)
+   
+ })
+
+drinksList.addEventListener("click", _=>{
+    verifyCheckedInput(drinks)
+    
+ })
+
+ dessertList.addEventListener("click" , _=>{
+    verifyCheckedInput(desserties)
+    
+ })
 
 
 
