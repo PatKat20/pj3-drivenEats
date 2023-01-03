be8f4879946cc2d4ed86cce3fe299da3440e3739
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-01-03 13:04:35 -0300

    Finaliza o envio do pedido pelo whatsapp

--- assets/js/products.js
+++ assets/js/products.js
@@ -2,50 +2,53 @@ const prodObj = {}
 
 prodObj.dishes = [
     {id:1, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
-    {id:2, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
-    {id:3, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
-    {id:4, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
+    {id:2, nome: "Salmao", desc: "Dalmao grelhado com legumes", price: 20.40, img: "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg" , type:"dish"},
+    {id:3, nome: "Omelete", desc: "Talvez nao seja omelete", price: 10.00, img: "https://cdn.pixabay.com/photo/2018/08/31/19/13/pumpkin-soup-3645375__340.jpg" , type:"dish"},
+    {id:4, nome: "Panqueca", desc: "Panquecas como almoço, delicia", price: 17.50, img: "https://cdn.pixabay.com/photo/2016/11/22/23/45/pancakes-1851249__340.jpg" , type:"dish"},
     {id:5, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
     {id:6, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
     {id:7, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
     {id:8, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
-    {id:9, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png" , type:"dish"},
-    {id:10, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png", type:"dish" },
-    {id:11, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png", type:"dish" },
-    {id:12, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png", type:"dish" },
-    {id:13, nome: "Frango Yin Yang", desc: "Um pouco de batata, um pouco de salada", price: 14.90, img: "assets/img/frango_yin_yang.png", type:"dish" },
+    
 ]
 
-prodObj.drinkes = [
-    {id:1, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:2, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:3, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:4, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:5, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:6, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:7, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
-    {id:8, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png",  type:"drink" },
+prodObj.drinks = [
+    {id:1, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 5.90, img: "assets/img/coca-cola.png", type:"drink" },
+    {id:2, nome: "Fanta Uva", desc: "Lata 350ml", price: 6.90, img: "http://rostiehouse.com.br/wp-content/uploads/2018/10/fanta-uva-lata.jpg", type:"drink" },
+    {id:3, nome: "Guarana Antartica", desc: "Lata 350ml", price: 7.90, img: "https://imgs.via.com.br/55019908/3g.jpg?imwidth=292", type:"drink" },
+    {id:4, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png", type:"drink" },
+    {id:5, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png", type:"drink" },
+    {id:6, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png", type:"drink" },
+    {id:7, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png", type:"drink" },
+    {id:8, nome: "Coquinha Gelada", desc: "Lata 350ml", price: 4.90, img: "assets/img/coca-cola.png", type:"drink" },
 ]
 
-prodObj.desserties = [
+prodObj.desserts = [
     {id:1, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
-    {id:2, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
-    {id:3, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
-    {id:4, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
-    {id:5, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
+    {id:2, nome: "Chocolate mousse", desc: "Chocolate mousse", price: 15.50, img:"https://img.delicious.com.au/Bxyg9oxN/del/2022/05/chocolate-mousse-with-cherry-ripe-168604-1.png", type: "dessert" },
+    {id:3, nome: "Bolinhos", desc: "Bolinhos diversos", price: 10.00, img:"https://media.istockphoto.com/id/497959594/photo/fresh-cakes.jpg?s=612x612&w=0&k=20&c=T1vp7QPbg6BY3GE-qwg-i_SqVpstyHBMIwnGakdTTek=", type: "dessert" },
+    {id:4, nome: "Brownie", desc: "Brownie de chocolate", price: 7.00, img:"https://insanelygoodrecipes.com/wp-content/uploads/2021/08/Gluten-Free-Chocolate-Chip-Brownies-683x1024.jpg", type: "dessert" },
+    {id:5, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Gluten-Free-Kahlua-Dessert_exps49655_THHC1997839C05_20_6bC_RMS.jpg", type: "dessert" },
+    {id:6, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
+    {id:7, nome: "Pudim", desc: "Apenas pudim", price: 7.90, img:"assets/img/pudim.png", type: "dessert" },
 ]
 
 prodObj.convertDateIntoHtml = productInfo => {
     return productInfo.reduce((accumulator, { id, nome: productName, desc, price: productPrice, img: productImg, type}) => {
        return accumulator += `
-        <label for="${type + id}" class="products" data-test="${type}">
+        <label for="${type + id}" 
+        class="products" 
+        data-test="${type}" 
+        data-itemname="${productName}"
+        data-itemprice="${productPrice}"
+        data-itemtype="${type}">
             <div class="imgArea">
-                <img src="${productImg}" alt="">
+                <img src="${productImg}" alt="" width="144px" height="82px">
             </div>
             <div class="descArea">
-                <h4 data-dishName="${productName}">${productName}</h4>
+                <h4 data-test="item-name">${productName}</h4>
                 <p class="desc">${desc}</p>
-                <p data-dishPrice="${productPrice}" class="price">${productPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                <p data-test="item-price" class="price">${productPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
             </div>
             <input type="radio" id="${type + id}" name="${type}" class="${type}">
         </label>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 // Seletores e Variaveis
+console.log(prodObj)
+
 const productList = document.querySelector("#dishesList")
 const drinkList = document.querySelector("#drinksList")
 const dessertList = document.querySelector("#dessertList")
 
 productList.innerHTML = prodObj.convertDateIntoHtml(prodObj.dishes)
-drinksList.innerHTML = prodObj.convertDateIntoHtml(prodObj.drinkes)
-dessertList.innerHTML = prodObj.convertDateIntoHtml(prodObj.desserties)
+drinksList.innerHTML = prodObj.convertDateIntoHtml(prodObj.drinks)
+dessertList.innerHTML = prodObj.convertDateIntoHtml(prodObj.desserts)
 
 // Renderizadores
 const dishes = document.querySelectorAll('.dish')
 const drinks = document.querySelectorAll(".drink")
-const desserties = document.querySelectorAll(".dessert")
+const desserts = document.querySelectorAll(".dessert")
 
 // Funções
+
+// Sempre que chamada ela vai verificar a lista dos produtos para saber quem está selecionado e quem não está
 const verifyCheckedInput = foodArray => {
     foodArray.forEach(input =>{
         if(input.checked){
@@ -23,23 +27,45 @@ const verifyCheckedInput = foodArray => {
     })
 }
 
+// Faz a verificação dos pratos selecionados e caso tenha 3 ele formata o botão de enviar
 const verifySelectedDishes = () =>{
-    const selecionados = document.querySelectorAll(".selecionado").length
     const sendButton = document.getElementById("sendButton")
     const sendButtonText = document.querySelector(".sendButtonText")
+    let contagemSelecionados = document.querySelectorAll(".selecionado").length
+    const urlEnconded = "https://wa.me/5521975109175?text=" + encodeURIComponent(getDishesInfo())
 
-    let itemInsert = selecionados === 2 ? `item` : `itens` 
-    sendButtonText.innerHTML = selecionados !== 3 ? `Selecione mais ${3 - selecionados} ${itemInsert} para fechar o produto` : `Fechar Pedido`
+    let itemInsert = contagemSelecionados === 2 ? `item` : `itens` 
+    sendButtonText.innerHTML = contagemSelecionados !== 3 ? `Selecione mais ${3 - contagemSelecionados} ${itemInsert} para fechar o produto` : `Fechar Pedido`
 
-    if(selecionados === 3){
+    if(contagemSelecionados === 3){
         sendButton.removeAttribute("disabled");
+        sendButton.setAttribute("href", urlEnconded)
         sendButton.style.backgroundColor = "#32B72F"
         sendButton.style.cursor = "pointer";
         sendButton.style.fontWeight = "700"
     }
 }
 
+// Pega os dados das receitas selecionadas e converte para uma mensagem
+const getDishesInfo = () => {
+    const selecionados = document.querySelectorAll(".selecionado")
+    const selecionadosParaArray = Array.from(selecionados)
+
+    const dishName = selecionadosParaArray[0] ? selecionadosParaArray[0].dataset.itemname : null
+    const drinkName = selecionadosParaArray[1]? selecionadosParaArray[1].dataset.itemname : null
+    const dessertName = selecionadosParaArray[2] ? selecionadosParaArray[2].dataset.itemname : null
+    let total = 0;
+    total = selecionadosParaArray.reduce((accumulator, product) => accumulator + Number(product.dataset.itemprice), 0)
+    
+    return `Olá, gostaria de fazer o pedido:
+    - Prato: ${dishName}
+    - Bebida: ${drinkName}
+    - Sobremesa: ${dessertName}
+Total: R$ ${total.toFixed(2)}`
+}
+
 // Eventos
+
 productList.addEventListener("click", _=>{
     verifyCheckedInput(dishes)
     verifySelectedDishes()
@@ -51,7 +77,7 @@ drinksList.addEventListener("click", _=>{
  })
 
  dessertList.addEventListener("click" , _=>{
-    verifyCheckedInput(desserties)
+    verifyCheckedInput(desserts)
     verifySelectedDishes()
  })
 
